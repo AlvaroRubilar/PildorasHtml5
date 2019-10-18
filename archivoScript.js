24686aa49bc9b3e43eecb50cc690f912905be81c
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-18 14:26:17 -0300

    video 14 error en saludo

--- archivoScript.js
+++ archivoScript.js
@@ -1,11 +1,11 @@
 function ejecuta() {
-//    document.querySelector("#principal p:last-child").onclick=saludo;
-var elementos=document.querySelectorAll("#principal p");
-for (let i = 0; i <elementos.length ; i++) {
-    
-    elementos[i].onclick;
-    
-}
+    //    document.querySelector("#principal p:last-child").onclick=saludo;
+    var elementos = document.querySelectorAll("#principal p");
+    for (let i = 0; i < elementos.length; i++) {
+
+        elementos[i].onclick = saludo;
+
+    }
 }
 
 function saludo() {
